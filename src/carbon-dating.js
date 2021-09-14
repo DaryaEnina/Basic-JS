a834fb667d457799be6cb8ffcc259f64135bcdd3
Gothica <semidevilish666@gmail.com>
2021-09-14 20:42:48 +0300

    start task

---
+++ src/carbon-dating.js
@@ -0,0 +1,32 @@
+import { NotImplementedError } from '../extensions/index.js';
+
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+
+/**
+ * Determine the age of archeological find by using
+ * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
+ *
+ * @param {String} sampleActivity string representation of current activity
+ * @return {Number | Boolean} calculated age in years or false
+ * in case of incorrect sampleActivity
+ *
+ * @example
+ *
+ * dateSample('1') => 22387
+ * dateSample('WOOT!') => false
+ *
+ */
+export default function dateSample(sampleActivity) {
+  if(!sampleActivity ||
+    typeof sampleActivity !== "string" ||
+    MODERN_ACTIVITY < sampleActivity ||
+    sampleActivity < 0) {
+    return false;
+  }
+  const activity = parseFloat(sampleActivity);
+  if(!activity) {
+    return false;
+  }
+  return Math.ceil(((Math.log(MODERN_ACTIVITY / activity)) / (Math.log(2)) * HALF_LIFE_PERIOD));
+};
